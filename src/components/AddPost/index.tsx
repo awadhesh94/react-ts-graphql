ce9fa1fe43ac6b6b0f5256c1b8552bd695fdc76e
awadhesh94 <awadhesh31st@outlook.com>
2023-04-15 14:26:43 +0530

    added new mutation which help to add new post

--- src/components/AddPost/index.tsx
+++ src/components/AddPost/index.tsx
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LinkProps } from '../../types/LinkListComponent';
+import { useMutation } from '@apollo/client';
+import { ADD_POST_MUTATION } from '../../Client/schemas/mutation';
 
 export const AddPostComponent = () => {
-  return <div className=''></div>;
+  const navigate = useNavigate();
+  const [addPost, setAddPost] = useState<LinkProps>({
+    description: '',
+    url: '',
+  });
+
+  const [addPostAction] = useMutation(ADD_POST_MUTATION, {
+    variables: {
+      description: addPost?.description,
+      url: addPost?.url,
+    },
+    onCompleted: () => navigate('/'),
+  });
+
+  return (
+    <div className='flex flex-col gap-4 justify-items-start p-5 w-fit items-start'>
+      <input
+        type='text'
+        placeholder='Enter description'
+        value={addPost?.description}
+        className='px-2 py-1 border-1 border-x-brand-biege'
+        onChange={(e) => {
+          setAddPost({ ...addPost, description: e.target.value });
+        }}
+      />
+      <input
+        type='text'
+        placeholder='Enter url'
+        value={addPost?.url}
+        className='px-2 py-1 border-1 border-x-brand-biege'
+        onChange={(e) => {
+          setAddPost({ ...addPost, url: e.target.value });
+        }}
+      />
+      <button className='bg-brand-light-green px-6 py-2' onClick={() => addPostAction()}>
+        Add
+      </button>
+    </div>
+  );
 };
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { LinkListComponent } from './LinkList';
+import { AddPostComponent } from './AddPost';
+import { Header } from './common/Header';
 
 export const App = () => {
   return (
-    <div className='flex'>
-      <LinkListComponent />
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<LinkListComponent />} />
+        <Route path='/add-post' element={<AddPostComponent />} />
+      </Routes>
     </div>
   );
 };
