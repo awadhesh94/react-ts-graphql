de225593193f96951a13437009034d9ce31c390b
awadhesh94 <awadhesh31st@outlook.com>
2023-04-15 13:49:01 +0530

    added fetch query

---
+++ src/components/AddPost/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AddPostComponent = () => {
+  return <div className=''></div>;
+};
--- src/components/LinkList/Link/index.tsx
+++ src/components/LinkList/Link/index.tsx
@@ -3,11 +3,9 @@ import { LinkProps } from '../../../types/LinkListComponent';
 
 export const LinkComponent: React.FC<LinkProps> = ({ description, url }) => {
   return (
-    <div key={url}>
-      <div>
-        <span>{description}</span>
-        <span>{url}</span>
-      </div>
+    <div className='grid m-1 px-8 py-4 bg-brand-coal text-brand-biege rounded-lg'>
+      <span className='font-bold'>{description}</span>
+      <p className='text-brand-yellow'>{url}</p>
     </div>
   );
 };
