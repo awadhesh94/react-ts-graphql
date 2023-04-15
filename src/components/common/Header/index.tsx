3581753f1d27c009fba57d3b3e3c4aeaa0259834
awadhesh94 <awadhesh31st@outlook.com>
2023-04-15 15:36:27 +0530

    Authentication code setup

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AUTH_TOKEN } from '../../../Client/constent';
 
 export const Header = () => {
+  const navigate = useNavigate();
+  const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
     <div className='px-4 py-2 bg-brand-yellow font-brandSemiBold'>
       <div className='flex justify-between'>
         <div className='flex gap-4'>
           <Link to='/'>Home</Link>
-          <div>|</div>
-          <Link to='/add-post'>Add Post</Link>
+          {authToken && (
+            <>
+              <div>|</div>
+              <Link to='/add-post'>Add Post</Link>
+            </>
+          )}
         </div>
-        <div>
-          <Link to='/'>Logout</Link>
-        </div>
+        {authToken ? (
+          <Link
+            to='/'
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              navigate('/');
+            }}
+          >
+            Logout
+          </Link>
+        ) : (
+          <Link to='/login'>Login</Link>
+        )}
       </div>
     </div>
   );
